82317c6abeb2ee6b703e16130f1009dfd7b288f6
Gerard Dethier <g.dethier@gmail.com>
2017-08-04 13:45:07 +0200

    Add debug logging when downloading dependencies.

--- src/gunilla/actions/download.py
+++ src/gunilla/actions/download.py
@@ -1,9 +1,32 @@
-from gunilla.config import instance
+from gunilla.config import instance as config_instance
+from gunilla.environment import instance as env_instance
 import json
 import os
 import requests
 import shutil
 from zipfile import ZipFile
+
+
+def run():
+    plugin_dependencies = config_instance().dependencies.plugins
+    for slug in plugin_dependencies:
+        dependency = plugin_dependencies[slug]
+        version = dependency.version
+        print("Downloading plugin '{}' at version '{}'".format(slug, version))
+
+        descriptor = json.loads(requests.get('https://api.wordpress.org/plugins/info/1.0/{}.json'.format(slug)).text)
+        if env_instance().debug:
+            print("Downloaded descriptor: %s" % json.dumps(descriptor, indent=2))
+        download_extract(slug, dependency, descriptor, 'plugins')
+
+    theme_dependencies = config_instance().dependencies.themes
+    for slug in theme_dependencies:
+        dependency = theme_dependencies[slug]
+        version = dependency.version
+        print("Downloading theme '{}' at version '{}'".format(slug, version))
+
+        descriptor = json.loads(requests.get('https://api.wordpress.org/themes/info/1.1/?action=theme_information&request[slug]={}&request[fields][versions]=true'.format(slug)).text)
+        download_extract(slug, dependency, descriptor, 'themes')
 
 
 def download_extract(slug, dependency, descriptor, folder):
@@ -22,23 +45,3 @@ def download_extract(slug, dependency, descriptor, folder):
     zip_file = ZipFile(zip_file_name)
     zip_file.extractall('{}'.format(folder))
     os.remove(zip_file_name)
-
-
-def run():
-    plugin_dependencies = instance().dependencies.plugins
-    for slug in plugin_dependencies:
-        dependency = plugin_dependencies[slug]
-        version = dependency.version
-        print("Downloading plugin '{}' at version '{}'".format(slug, version))
-
-        descriptor = json.loads(requests.get('https://api.wordpress.org/plugins/info/1.0/{}.json'.format(slug)).text)
-        download_extract(slug, dependency, descriptor, 'plugins')
-
-    theme_dependencies = instance().dependencies.themes
-    for slug in theme_dependencies:
-        dependency = theme_dependencies[slug]
-        version = dependency.version
-        print("Downloading theme '{}' at version '{}'".format(slug, version))
-
-        descriptor = json.loads(requests.get('https://api.wordpress.org/themes/info/1.1/?action=theme_information&request[slug]={}&request[fields][versions]=true'.format(slug)).text)
-        download_extract(slug, dependency, descriptor, 'themes')
